115bc19e14beed00934695beed8694e714cc5b19
arkadiysotnikov <cotnikoarkady@gmail.com>
2024-02-02 16:32:24 +0400

    feat(index): first route

--- main.go
+++ main.go
@@ -1,1 +1,25 @@
+package main
 
+import (
+	"net/http"
+	"os"
+)
+
+// handlers in Go defining controllers
+// so it carry out application logic and writing response headers and bodies
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Hello world!</h1>"))
+}
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	// mux equal to the router in general terms
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", indexHandler)
+	http.ListenAndServe(":"+port, mux)
+}
